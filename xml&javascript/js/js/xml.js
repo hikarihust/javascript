92237c485be59412ef4a1e2071708f73804ccd7c
vudinhquang <vudinhquangk53@gmail.com>
2019-10-05 13:56:35 +0900

    XML JavaScript - Ajax load

---
+++ xml&javascript/js/js/xml.js
@@ -0,0 +1,25 @@
+function loadFile(url, method, type) {
+    // Khởi tạo đối tượng XMLHttpRequest
+    if (window.XMLHttpRequest) {
+        var xhttp = new XMLHttpRequest();
+    } else { // ie5 -ie6
+        var xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    // Xác định phương thức và tập tin chúng ta muốn lấy thông tin
+    // Phương thức GET = POST
+    // xhttp.open("GET", "files/002.html", false);
+    xhttp.open(method, url, false);
+
+    // Gửi yêu cầu đến máy chủ
+    xhttp.send();
+
+    // Lấy nội dung trả về từ máy chủ
+    if (type === "text") {
+        var val = xhttp.responseText;
+    } else {
+        var val = xhttp.responseXML;
+    }
+
+    return val;
+}
